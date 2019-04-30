447ebd287859a3d89faa1a5fc97839c24aeefc74
madewithsmiles <jndiarrassouba@gmail.com>
2019-04-30 07:06:16 -0500

    Added Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-footer absolute>
+      <div class="myfooter">
+        Made with ❤️ by Omicron Wow &copy; {{new Date().getFullYear() }}
+      </div>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+.myfooter {
+    background-color: white; 
+    color: white; 
+    padding: 10px; 
+    bottom: 0; 
+    align-content: center; 
+    background-image: linear-gradient(to right, #20c997, #17a2b8);
+}
+</style>
